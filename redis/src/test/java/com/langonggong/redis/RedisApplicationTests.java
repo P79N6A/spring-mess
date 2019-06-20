c13b3b06d874c920a9e8259a1939e13395c2454a
lanxu <lanxu@kuaishou.com>
2019-06-20 16:49:45 +0800

    t

--- redis/src/test/java/com/langonggong/redis/RedisApplicationTests.java
+++ redis/src/test/java/com/langonggong/redis/RedisApplicationTests.java
@@ -1,14 +1,12 @@
 package com.langonggong.redis;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 public class RedisApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println(1);
     }
 
 }
